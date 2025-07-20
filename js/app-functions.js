6af196be07b60c7218267987eb460801c2ec0fd2
Muslimjon Kholjuraev <muslimjonkholjuraev@muslimjons-macbook-air.home>
2025-07-20 12:53:20 +0200

    [LANGUAGE]: disable default language application

--- js/app-functions.js
+++ js/app-functions.js
@@ -70,29 +70,29 @@
 	sitePlusMinus();
 })()
 
-window.addEventListener("DOMContentLoaded", applyGeoLanguage);
+// window.addEventListener("DOMContentLoaded", applyGeoLanguage);
 
 document.getElementById("contactUsButton").addEventListener('click', () => {
 	document.getElementById('footer-section').scrollIntoView({ behavior: 'smooth' });
 });
 
 
-function applyGeoLanguage () {
-		console.log('country', 'debugg')
-	fetch("https://ipwho.is/")
-	.then(res => res.json())
-	.then(data => {
-		const country = data.country_code;
-		console.log(country, 'debugg')
-		switch (country) {
-		// case "RU":
-		// 	window.location.href = "./ru.html";
-		// 	break;
-		case "UZ":
-			window.location.href = "languages/uz.html";
-			break;
-		default:
-			window.location.href = "index.html";
-		}
-	});
-}
+// function applyGeoLanguage () {
+// 		console.log('country', 'debugg')
+// 	fetch("https://ipwho.is/")
+// 	.then(res => res.json())
+// 	.then(data => {
+// 		const country = data.country_code;
+// 		console.log(country, 'debugg')
+// 		switch (country) {
+// 		// case "RU":
+// 		// 	window.location.href = "./ru.html";
+// 		// 	break;
+// 		case "UZ":
+// 			window.location.href = "languages/uz.html";
+// 			break;
+// 		default:
+// 			window.location.href = "index.html";
+// 		}
+// 	});
+// }
